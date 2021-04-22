966adb99a4c3ecdbd65123574857b7178ef99be1
Andrea Rojas <andrear.2810@gmail.com>
2021-04-21 19:37:56 -0500

    Project finished

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', score: 2 },
+  { id: 12, name: 'Narco', score: 7 },
+  { id: 13, name: 'Bombasto', score: 10 },
+  { id: 14, name: 'Celeritas', score: 12 },
+  { id: 15, name: 'Magneta', score: 4 },
+  { id: 16, name: 'RubberMan', score: 15 },
+  { id: 17, name: 'Dynama', score: 13 },
+  { id: 18, name: 'Dr IQ', score: 11 },
+  { id: 19, name: 'Magma', score: 16 },
+  { id: 20, name: 'Tornado', score: 9 }
+];
